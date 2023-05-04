921fb5eb16387ba0334cf2c28ea7f63b703bf269
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-05-04 22:06:36 +0200

    Docker build front & back

---
+++ frontend/frontend_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set +e
+docker network create -d bridge momo_network || true
+docker pull gitlab.praktikum-services.ru:5050/std-012-056/momo-store/momo-frontend:latest
+docker stop momo-frontend || true
+docker rm momo-frontend || true
+set -e
+docker run -d --name momo-frontend \
+    --network=momo_network \
+    --restart always \
+    --pull always \
+    --env-file .env \
+    gitlab.praktikum-services.ru:5050/std-012-056/momo-store/momo-frontend:latest
