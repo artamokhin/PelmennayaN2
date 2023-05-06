84f4c8d7f2ce2b411fa1c95c21f70fbff7e7a2cf
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-05-06 18:51:22 +0200

    Try to tune nginx conf to connect to backend

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -6,7 +6,7 @@ docker stop momo-backend || true
 docker rm momo-backend || true
 set -e
 docker run -d --name momo-backend \
-    --network=momo_network -p 8081:8081\
+    --network=momo_network -p 8081\
     --restart always \
     --pull always \
     gitlab.praktikum-services.ru:5050/std-012-056/momo-store/momo-backend:latest 
