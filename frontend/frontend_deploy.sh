fd0baad8802c0cba525f9c6c6d4faf81095932d4
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-05-07 13:30:18 +0200

    Try to tune nginx conf to connect to backend

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -6,7 +6,7 @@ docker stop momo-frontend || true
 docker rm momo-frontend || true
 set -e
 docker run -d --name momo-frontend \
-    --network=momo-network -p 81:80\
+    --network=momo-network -p 80:80\
     --restart always \
     --pull always \
     gitlab.praktikum-services.ru:5050/std-012-056/momo-store/momo-frontend:latest
