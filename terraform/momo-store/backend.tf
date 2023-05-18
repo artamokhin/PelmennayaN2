969916e1e45d2335cca326d822e1e87ebdcf909a
Artamokhin Aleksandr <artamokhin1@yandex.ru>
2023-05-18 11:54:18 +0200

    add terraform state to S3 bucket

--- terraform/momo-store/backend.tf
+++ terraform/momo-store/backend.tf
@@ -1,4 +1,13 @@
 terraform {
-  backend "http" {
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-momo-store"
+    region     = "ru-central1"
+    key        = "Momo-store.tfstate"
+    access_key = "***REMOVED***"
+    secret_key = "***REMOVED***"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
   }
 }
